e20c8a81a0ddd7199086af4f88698e6847d60b7f
Pro100Paladin <pro100paladin@icloud.com>
2024-02-20 19:26:21 +0300

    finish commit

--- src/Story.java
+++ src/Story.java
@@ -5,8 +5,8 @@ public class Story {
 
     private void setListToys() {
 
-        String[] names = {"мяч", "скакалка", "пазлы", "кубики", "конструктор"};
-        int[] frequencys = {5, 4, 3, 2, 1};
+        String[] names = {"мяч", "скакалка", "пазлы", "кубики", "конструктор", "машинка", "кукла", "самолет"};
+        int[] frequencys = {8, 7, 6, 5, 4, 3, 2, 1};
         for (int i = 0; i < names.length; i++) {
             Toy toy = new Toy(names[i], frequencys[i] * 10, frequencys[i]);
             listToys.add(toy);
