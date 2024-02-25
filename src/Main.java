0efbb45182640e9000e07551813a928a075d1022
Pro100Paladin <pro100paladin@icloud.com>
2024-02-25 18:06:34 +0300

    rename

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        RaffledToys.deletingEmptyLines();
+        RaffledToys.renameFileToysList();
         myMenu.menu();
     }
 }
--- src/RaffledToys.java
+++ src/RaffledToys.java
@@ -1,5 +1,9 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -31,27 +35,55 @@ public class RaffledToys {
 
     }
 
-    public static void deleteRaffleToys(String lineContent)  {
+    public static void deleteRaffleToys(String lineContent) {
         File file = new File("src/ToysList.txt");
         File temp = new File("_temp_");
         try {
             PrintWriter out = new PrintWriter(new FileWriter(temp), false);
+
             Files.lines(file.toPath())
                     .filter(line -> !line.contains(lineContent))
                     .forEach(out::println);
             out.close();
             temp.renameTo(file);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public static void deletingEmptyLines() throws IOException {
+        String inputFileName = "src/ToysList.txt";
+        String outputFileName = "src/_temp_";
+
+        try (BufferedReader inputFile = new BufferedReader(new FileReader(inputFileName));
+             PrintWriter outputFile = new PrintWriter(new FileWriter(outputFileName))) {
+            String lineOfText;
+            while ((lineOfText = inputFile.readLine()) != null) {
+                lineOfText = lineOfText.trim();
+                if (!lineOfText.isEmpty()) {
+                    outputFile.println(lineOfText);
+                }
+
+            }
+        }
+
+    }
+    public static void renameFileToysList(){
+        Path sourcePath = Paths.get("src/_temp_");
+        Path destinationPath = Paths.get("src/ToysList.txt");
+        try {
+            Files.move(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            System.out.println("Ошибка! Возникло исключение: " + e.getMessage());
+        }
+    }
 
     ArrayList<Toy> getListToys() throws Exception {
         readRaffledToys();
         return listToys;
-    }
 
+    }
 }
 
 
